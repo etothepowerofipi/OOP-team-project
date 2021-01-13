2617e7e7eb500298a26fa26fb8ccbf721b81a8c7
askertsos <aggelosskertsos@gmail.com>
2021-01-13 19:41:14 +0200

    fae poutso

--- Map.cpp
+++ Map.cpp
@@ -1,11 +1,28 @@
-#include "Map.h"
 #include <iostream>
 #include <string>
 #include <ctime>
+#include "Map.h"
 
 using namespace std;
 
 Map::Map(){
-    int numofmarkets=rand()%15;  //Min marketplaces=10 and max marketplaces=15
-    if(numofmarkets)
+    for(int i=0;i<10;i++){
+        for(int j=0;j<10;j++)
+            grid[i][j]='C';
+    }
+    int numofmarkets=10+rand()%6;  //Paragw enan tuxaio ari8mo anamesa sto Min marketplaces=10 kai max marketplaces=15
+    int i,j;
+    while(numofmarkets > 0){
+        i=rand()%10;
+        j=rand()%10;
+        grid[i][j]='M';
+    }
+}
+
+void Map::print(){
+    for(int i=0;i<10;i++){
+        for(int j=0;j<10;j++)
+            cout << grid[i][j];
+        cout << endl;
+    }
 }
