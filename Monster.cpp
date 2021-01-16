53a3b10a912a69def3978142a21ef5d4729dab93
etothepowerofipi <nikwnbibis@gmail.com>
2021-01-16 22:03:20 +0200

    .

--- Monster.cpp
+++ Monster.cpp
@@ -83,12 +83,13 @@ Monster* monsterGenerator(const int heroAverage){
     switch (type){
         case 0:
             monster = new Dragon(genName("monster"),randomLevel(heroAverage));
-            return monster;
+            break;
         case 1:
             monster = new Exosceleton(genName("monster"),randomLevel(heroAverage));
-            return monster;
+            break;
         case 2:
             monster = new Spirit(genName("monster"),randomLevel(heroAverage));
-            return monster;
+            break;
     }
+    return monster;
 }
